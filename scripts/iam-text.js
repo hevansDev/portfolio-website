f31c3ea4814e98bff080e2cfed06fca85df58cf9
Hugh Evans <hugh.evans@dae.mn>
2022-12-21 17:56:29 +0000

    feat(page): Added some content to landing page

---
+++ scripts/iam-text.js
@@ -0,0 +1,9 @@
+const iams = ['maker', 'environment lead', 'probably planning an event', 'working on a blog', 'former apprentice'];
+var pointer = 0
+setInterval(function() {
+    document.getElementById('iam-text').innerHTML=`I am IT consultant and ${iams[pointer]}.`;
+    pointer++;
+    if (pointer > iams.length - 1) {
+        pointer = 0;
+    }
+  }, 5000);
