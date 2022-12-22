8f395828a81572fe55fa1bf0eecc9257515929f5
Hugh Evans <hugh.evans@dae.mn>
2022-12-22 11:08:35 +0000

    fix(pages): Removed iam script from new pages

--- scripts/iam-text.js
+++ scripts/iam-text.js
@@ -1,7 +1,7 @@
 const iams = ['maker', 'environment lead', 'probably planning an event', 'working on a blog', 'former apprentice'];
 var pointer = 0
 setInterval(function() {
-    document.getElementById('iam-text').innerHTML=`I am IT consultant and ${iams[pointer]}.`;
+    document.getElementById('iam-text').innerHTML=`I am an IT consultant and ${iams[pointer]}.`;
     pointer++;
     if (pointer > iams.length - 1) {
         pointer = 0;
