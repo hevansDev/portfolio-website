384105961d97d5ac1b99d0b8d0d0db86a27b7c70
hevansDev <hugh.evans.dev@gmail.com>
2023-03-06 22:01:09 +0000

    fix(site): Hid old version of site

--- scripts/iam-text.js
+++
@@ -1,9 +0,0 @@
-const iams = ['maker', 'environment lead', 'probably planning an event', 'working on a blog', 'former apprentice'];
-var pointer = 0
-setInterval(function() {
-    document.getElementById('iam-text').innerHTML=`I am an IT consultant and ${iams[pointer]}.`;
-    pointer++;
-    if (pointer > iams.length - 1) {
-        pointer = 0;
-    }
-  }, 5000);
